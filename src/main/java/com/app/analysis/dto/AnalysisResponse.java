fc7ead16f2320141b75c42a54c7218c14e3c7eae
Doyeon Kim <ehdus9809@gmail.com>
2025-08-30 22:04:21 +0900

    [feat] analysis dto 작성

---
+++ src/main/java/com/app/analysis/dto/AnalysisResponse.java
@@ -0,0 +1,16 @@
+package com.app.analysis.dto;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record AnalysisResponse (
+    double remainingPercent,
+    double remainingMg,
+    double hoursToZero,
+    LocalDateTime estimatedSleepTime,
+    List<Point> series,
+    String comment
+    ) {
+    public record Point(LocalDateTime time, double mg) {}
+}
