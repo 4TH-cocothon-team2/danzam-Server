7076dcd2f79eed1ba48f2661240bfa73ca9dfd35
Doyeon Kim <ehdus9809@gmail.com>
2025-08-30 22:58:50 +0900

    [Refactor] 메인 클래스에 스캔 범위 지정

--- src/main/java/com/example/danzam/danzamApplication.java
+++ src/main/java/com/example/danzam/danzamApplication.java
@@ -2,10 +2,13 @@ package com.example.danzam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.danzam", "com.app.analysis"})
+@EnableJpaRepositories(basePackages = {"com.app.analysis"})
+@EntityScan(basePackages = {"com.app.analysis"})
 public class danzamApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(danzamApplication.class, args);
 	}
