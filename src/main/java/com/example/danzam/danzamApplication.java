93548976cf35d8f336d89a5265a0ebb08fe8e579
Doyeon Kim <ehdus9809@gmail.com>
2025-08-30 19:54:53 +0900

    [Rename] 파일명 danzam으로 변경

--- src/main/java/com/example/danzam/danzamApplication.java
+++ src/main/java/com/example/danzam/danzamApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.danzam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class danzamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(danzamApplication.class, args);
 	}
 
 }
